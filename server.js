15d1433518117e12d0e5389296a63397a85a9252
esbennn <esbenlykkeolsen@gmail.com>
2018-04-10 11:08:28 +0200

    8080

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var port = process.env.OPENSHIFT_NODEJS_PORT || 1337;
+var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 var ip =  process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
 
 var express = require('express');
